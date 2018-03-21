3d6ebc22a04f403cc3845fd0c7dfbe32dac62d02
Arthur Arts <arart@fundeon.nl>
2018-03-21 14:53:45 +0100

    first commit

---
+++ src/main/java/go/pacmen/ing/nl/pacmengo/PacmenGoApplication.java
@@ -0,0 +1,12 @@
+package go.pacmen.ing.nl.pacmengo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacmenGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacmenGoApplication.class, args);
+	}
+}
